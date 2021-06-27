8d7ceccfac1c6328116c34fb6797ec1e07619965
KoKoLeeee <jaredlee33@hotmail.com>
2021-06-27 14:13:20 +0800

    added more contacts

--- src/components/official/emergencyContact.vue
+++ src/components/official/emergencyContact.vue
@@ -9,25 +9,25 @@
         <li v-for="(record, item) in contacts" :key="item">
           <container>
             <v-card>
-                <v-card-title>
-                    <div class="left">
-                        {{record.contactname}}
-                        <br>
-                        {{record.contactnum}}
-                    </div>
+              <v-card-title>
+                <div class="left" style="font-size: 15px">
+                  {{ record.contactname }}
+                </div>
 
-                    <div class="right" style="font-size:25px">
-                        <i class="fas fa-phone-alt"></i>
-                    </div>
-                </v-card-title>
+                <div class="right" style="font-size: 25px">
+                  <i class="fas fa-phone-alt"></i>
+                </div>
+              </v-card-title>
 
-                <v-divider class="mx-4"></v-divider>
+              <v-divider class="mx-4"></v-divider>
 
-                <v-card-text>
-                  {{record.contactdescription}}
-                </v-card-text>
+              <v-card-text style="text-align: left">
+                {{ record.contactinfo }}
+                {{ record.emergencynum }}
+                <br />
+                {{ record.contacthrs }}
+              </v-card-text>
             </v-card>
-            
           </container>
         </li>
       </ul>
@@ -42,16 +42,27 @@ export default {
     return {
       contacts: [
         {
-          contactname: 'Singapore Embassy (US)',
-          contactnum: '+1-202-537-3100',
-          contactdescription: 'Need help any overseas? Contact Singapore Embassy now!'
+          contactname:
+            "Embassy of the Republic of Singapore - United States of America",
+          contactinfo: "Tel: +1-202-537-3100",
+          contacthrs:
+            "Operating Hours: Mon - Fri | 8.30 am to 1.00pm / 2.00 pm to 5.00 pm | Closed on Weekends, US Federal Holidays and 9 August",
         },
         {
-          contactname: 'Mayaguhan',
-          contactnum: '+65 6123 5678',
-          contactdescription: 'Call bodoh kambing susu'
-        }
-      ]      
+          contactname:
+            "Consulate of the Republic of Singapore - United States of America (New York)",
+          contactinfo: "Tel: +1-212-223-3331",
+          contacthrs:
+            "Operating Hours: Mon - Fri | 9.00 am to 1.00 pm | Closed on weekends and holidays | No walk-ins allowed. All consular services by appointment only.",
+        },
+        {
+          contactname:
+            "Consulate-General of the Republic of Singapore - United States of America (San Francisco)",
+          contactinfo: "Tel: +1-415-543-4775",
+          contacthrs:
+            "Operating Hours:  - Fri | 8.30 am to 1.00 pm / 2.00 pm to 5.00 pm | Closed on Weekends, US Federal Holidays and 9 August",
+        },
+      ],
     };
   },
 };
@@ -72,11 +83,11 @@ ul {
 }
 
 .left {
-    width: 90%;
-    text-align: left;
+  width: 80%;
+  text-align: left;
 }
 .right {
-    float: right;
+  float: right;
+  width: 20%;
 }
-
 </style>
